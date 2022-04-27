7381fa5e605dbc517d5e1f7367231e8491f3123d
Mads <mads.po.ped@gmail.com>
2022-04-27 08:15:52 +0200

    Add docker-compose.yml + experiment with error 0 in index.js (comment out now)

--- 3_visits_webapp/index.js
+++ 3_visits_webapp/index.js
@@ -1,14 +1,22 @@
+const set_port = 8081;
+const redis_port = 6379;
+
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
-const client = redis.createClient();
-const set_port = 8081;
+const client = redis.createClient({
+    host: "redis-server",
+    port: redis_port
+});
+client.set("visits", 0);
 
 app.get("/", (req, res) => {
+    // process.exit(0);
     client.get("visits", (err, visits) => {
         res.send("Number of visits is " + visits);
-        client.set("visits", parseInt(visits) + 1)
+        client.set("visits", parseInt(visits) + 1);
     });
 });
 
